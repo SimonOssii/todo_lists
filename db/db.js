8beb80c48f2adf3d699c7c91e7149169a6436115
Simon <Simon@gmail.com>
2012-09-18 09:30:53 +0800

    更改資料庫樣式，資料重新抓取導向沒問題

--- db/db.js
+++ db/db.js
@@ -1,15 +1,47 @@
-var records = require("mongoose");
+//~ var records = require("mongoose");
+//~ 
+//~ var records_Schema   = records.Schema;
+//~ 
+//~ var todolist = new records.Schema({
+  //~ title: String,
+  //~ date:String,
+  //~ content:String,
+  //~ done:String
+//~ });
+//~ 
+//~ 
+//~ 
+//~ records.model( 'todolist', todolist );
+//~ records.connect('mongodb://localhost/Todo');
+//~ 
+//~ exports.todolist = records;
 
+
+
+
+
+var records = require("mongoose");
 var records_Schema   = records.Schema;
 
-var todolist = new records.Schema({
-  title: String,
-  date:String,
-  content:String,
-  done:String
+//~ var todo_list = new records.Schema({
+  //~ title: String,
+  //~ date:String,
+  //~ content:String,
+  //~ done:String
+//~ });
+
+var todo_list = new records.Schema({
+    user:String,
+    item:{
+        title:String,
+        desc:String,
+        expire_date:String
+    },
+    create_date:String,
+    status:String
 });
 
-records.model( 'todolist', todolist );
-records.connect('mongodb://localhost/Todo');
+records.model( 'todo_list', todo_list );
+records.connect('mongodb://localhost/todo_lists');
 
-exports.todolist = records;
+exports.todo_list = records;
--- routes/index.js
+++ routes/index.js
@@ -5,20 +5,23 @@
  
 var mongoose = require( 'mongoose' )
 var db = require('../db/db.js');
-var todolist = db.todolist.model( 'todolist');
+var todolist = db.todo_list.model( 'todo_list');
 
 exports.index = function(req, res){
      todolist.
         find(function ( err, list ){
-                    console.log(list)
+            console.log(list)
             res.render('index', {list:list});
         });
         //res.render('index');
 };
 
 exports.add = function(req, res){
+        console.log(req.body.add_title)
         new todolist({
-            title:req.body.add_title
+            item:{
+                title:req.body.add_title
+            }
         }).save();
         res.redirect( '/');
 
@@ -35,8 +38,9 @@ exports.searchList = function(req,res){
 exports.change = function(req,res){
     todolist.
     findByIdAndUpdate(
-        req.body.getid, 
-        {title:req.body.change_title,date:req.body.change_date,content:req.body.change_content},
+        req.body.getid,
+        {item:{title:req.body.change_title,desc:req.body.change_desc,expire_date:req.body.change_date}},
+        //~ {title:req.body.change_title,date:req.body.change_date,content:req.body.change_content},
         {upsert:true},
         function(err){
             console.log(err);    
